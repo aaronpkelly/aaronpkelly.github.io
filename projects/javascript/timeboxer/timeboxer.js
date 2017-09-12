0669d3dbb73fe5d9bdf97cedf1fb86c8dd202d6e
Aaron Kelly <aaron.paul.kelly@gmail.com>
2017-09-12 13:15:25 +0000

    major updates to timeboxer

--- projects/javascript/timeboxer/timeboxer.js
+++ projects/javascript/timeboxer/timeboxer.js
@@ -1,7 +1,3 @@
-// TODO
-// - spamming the alarm button after alarms are already created causes chaos.
-// need to reset state back to beginning
-
 /*
 TIMELEFT=int(sys.argv[1])
 items = ("make bed", "clean bathroom", "finish dishes", "sweep floors")
@@ -13,17 +9,19 @@ for i, item in enumerate(items):
 var ALARM_DELTA = 500; // the amount of difference we can forgive
 var ALARM_DELTA_EARLY = 60000 * 5; // 1 min
 var AUDIO = true;
+var COOKIE_EXPIRY_IN_DAYS = 30;
 var MS_PER_MINUTE = 60000; // 1 min
 // var alarm = new Audio('alarm_guitarReverb.flac');
 // var alarmEarly = new Audio('alarmWarning_guitarCinematic.flac');
 var alarms_totalAllocationTime = 60;
 var alarms_deadline = new Date()
+var alarms_start= new Date()
 var alarms = [];
 var currentTime = new Date();
 var isRunning = false;
 var playedAlarms = [];
 var table_alarms = document.getElementById("table_alarms");
-//var tasks = []
+var tasks = [] // used for cookie session storage
 var audio_tick;
 var audio_alarm;
 var audio_alarm_early;
@@ -64,7 +62,7 @@ function checkUserInput() {
     var key = window.event.keyCode;
 
     if (key == 13) {
-        addTask();
+        newElement("newElement");
     }
 }
 
@@ -122,7 +120,7 @@ function redraw() {
     currentTime = new Date();
 
     document.getElementById("stdout_time").innerHTML = "current time: " + convertDateToString(currentTime);
-    document.getElementById("stdout_deadline").innerHTML = "deadline: " + convertDateToString(alarms_deadline);
+    document.getElementById("stdout_deadline").innerHTML = "complete all tasks by: " + convertDateToString(alarms_deadline);
 
     // console.log(convertDateToString(currentTime));
 
@@ -140,15 +138,19 @@ function redraw() {
 
 function regenerateAlarms() {
     var myNodeList = document.getElementsByTagName("LI");
+    var myUL = document.getElementById("myUL");
     clearAlarmState();
     var timeAvailableInMS = alarms_deadline.getTime() - currentTime.getTime() ;
     var timePerTask = timeAvailableInMS / myNodeList.length;
 
+
+
     for (var i = 0; i < myNodeList.length; i++) {
         // var newDateObj = new Date(alarms_totalAllocationTime + ((timePerTask * (i+1)) * MS_PER_MINUTE));
-        var nextAlarm = currentTime.getTime() + (timePerTask * (i+1));
-
+        var nextAlarm = alarms_start.getTime() + (timePerTask * (i+1));
+        console.log(currentTime.getTime());
         var newDateObj = new Date(nextAlarm);
+        console.log(newDateObj.getTime());
         alarms.push(newDateObj);
     }
 
@@ -157,13 +159,23 @@ function regenerateAlarms() {
     var table_alarms = document.getElementById("table_alarms");
 
     for (var i = 0; i < alarms.length; i++) {
-        var str = "task " + i + " must be completed by: "
-            + (alarms[i].getHours() < 10 ? '0':'') + alarms[i].getHours()
-            + ":"
-            + (alarms[i].getMinutes() < 10 ? '0':'') + alarms[i].getMinutes()
-            + "\r\n";
+	if (i == 0) {
+		var str = (alarms[i].getHours() < 10 ? '0':'') + alarms[i].getHours()
+			+ ":"
+			+ (alarms[i].getMinutes() < 10 ? '0':'') + alarms[i].getMinutes()
+			+ "\r\n";
+	} else {
+		var str = "then: "
+			+ (alarms[i].getHours() < 10 ? '0':'') + alarms[i].getHours()
+			+ ":"
+			+ (alarms[i].getMinutes() < 10 ? '0':'') + alarms[i].getMinutes()
+			+ "\r\n";
+	}
         var row = table_alarms.insertRow(i);
         var cell = row.insertCell(0);
+	if (i == 0) {
+		cell.style.fontSize = "xx-large";
+	}
         cell.innerHTML = str;
     }
 }
@@ -180,10 +192,19 @@ function regenerateTasks() {
     }
 }
 
+function session_clear() {
+	alert("please clear cookies manually for now");
+}
+
+function session_save() {
+    setCookie("tasks_cookie", tasks, COOKIE_EXPIRY_IN_DAYS);
+}
+
 function setDeadline() {
     var timeAllocatedInMins = document.getElementById("textarea_timeAvailable").value
     console.log(timeAllocatedInMins)
     alarms_deadline = new Date(currentTime.getTime() + (timeAllocatedInMins * MS_PER_MINUTE));
+    alarms_start = new Date(currentTime.getTime());
 }
 
 function setDeadlineAndGo() {
@@ -225,27 +246,6 @@ function splitTextareaIntoString() {
 // a very nice task manager by W3Schools
 // -------------------------------------
 
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
@@ -255,30 +255,58 @@ list.addEventListener('click', function(ev) {
 }, false);
 
 // Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("input-tasks").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    // alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("input-tasks").value = "";
+function newElement(element_name) {
+	if (element_name != "newElement") {
+		var inputValue = element_name;
+		console.log("creating" + element_name);
+	} else {
+		var inputValue = document.getElementById("input-tasks").value;
+	}
+	var t = document.createTextNode(inputValue);
+	var li = document.createElement("li");
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+	// create a 'li' item and append a textnode containing the name of the
+	// task. this variable is needed by the onclick function below to help
+	// find and remove the task when it is deleted with the 'X'
+	li.id = inputValue;
+	li.appendChild(t);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+	if (inputValue === '') {
+	    // alert("You must write something!");
+	} else {
+	    document.getElementById("myUL").appendChild(li);
+	    tasks.push(inputValue) // for saving in a cookie
+	    console.log(tasks)
+	}
+	document.getElementById("input-tasks").value = "";
+
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	li.appendChild(span);
+
+	span.onclick = function() {
+		var div = this.parentElement;
+		var elem = document.getElementById(li.id);
+		div.style.display = "none";
+		elem.parentNode.removeChild(elem);
+	}
+
+	/*
+	// Click on a close button to hide the current list item
+	var close = document.getElementsByClassName("close");
+	var i;
+	for (i = 0; i < close.length; i++) {
+		close[i].onclick = function() {
+			var div = this.parentElement;
+			div.style.display = "none";
+
+			var elem = document.getElementById(li.id);
+			elem.parentNode.removeChild(elem);
+		}
+	}
+	*/
 }
 
 // cookies! this feature only works online (will not work for local pages)
@@ -307,6 +335,7 @@ function getCookie(cname) {
 }
 
 function checkCookie() {
+    /*
     var user=getCookie("username");
     if (user != "") {
         alert("Welcome again " + user);
@@ -316,9 +345,20 @@ function checkCookie() {
            setCookie("username", user, 30);
        }
     }
+    */
+    var tasks_cookie = getCookie("tasks_cookie");
+    if (tasks_cookie != "") {
+	// recreate task elements
+	tasks_cookie_split = tasks_cookie.split(",");
+    	for (var i = 0; i < tasks_cookie_split.length; i++) {
+		newElement(tasks_cookie_split[i]);
+	}
+    }
 }
 
+
 window.onload = function(){
-    createAudioElements();
-    setInterval("redraw()", 1000);
+	checkCookie();
+	createAudioElements();
+	setInterval("redraw()", 1000);
 }
