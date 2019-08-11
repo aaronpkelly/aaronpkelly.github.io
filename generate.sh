93f5cc03514850d1a200d5297d45911dfe27a7cc
Aaron Kelly <aaronkelly@fastmail.com>
2019-08-11 10:33:52 +0000

    adding before update

--- generate.sh
+++ generate.sh
@@ -1,13 +1,41 @@
 #!/usr/bin/env bash
 # script for generating an index.md, which github pages automatically publishes
-set -x
 
-cat README.md > index.md
-printf "\n" >> index.md
+addReadme() {
+	cat README.md >> index.md
+}
 
-for file in posts/*.md; do
-	echo "[${file}](${file})" >> index.md
-	echo "" >> index.md
-done
+main() {
+	zeroOutIndexFile
+	addReadme
+	generateTOCs
+	generatePosts
+	generatePostList
+}
 
-set +x
+generatePosts() {
+	for file in posts/*.md; do
+		cat "$file" >> posts/withTocs/"${file}"
+	done
+}
+
+generatePostList() {
+	for file in posts/withTOCs/*.md; do
+		echo "[${file}](${file})" >> index.md
+		echo "" >> index.md
+	done
+}
+
+generateTOCs() {
+	mkdir -p posts/withTOCs
+
+	for file in posts/*.md; do
+		./gh-md-toc "$FILE" > posts/withTOCs/"${file}"
+	done
+}
+
+zeroOutIndexFile() {
+	:>| index.md
+}
+
+main
